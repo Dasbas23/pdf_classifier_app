cf21e34349a128d6b87b06622591e2db6d7c84b8
Marius Dodu <dasbas23@gmail.com>
2026-02-20 03:27:24 +0100

    Actualización y mejora proveedores.json

--- test_debug.py
+++ test_debug.py
@@ -7,7 +7,7 @@ from app.core.parser import analizar_documento
 
 # --- CONFIGURACIÓN ---
 # Pon aquí la ruta de tu PDF real de prueba
-RUTA_PDF_PRUEBA = r"C:\Users\Marius\Downloads\pag1.pdf"
+RUTA_PDF_PRUEBA = r"C:\Users\marius.ion\Downloads\SVIMPRESION19_ZARAGOZA_OFICINA TECNICA_1450_001.pdf"
 
 
 # Colores para la consola (Opcional, pero mola)
